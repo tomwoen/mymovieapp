38f8e9f000823c1d7c17e1580e9352338325168c
tomwoen <thomas.owen@qa.com>
2018-04-25 15:16:08 +0100

    Created JSON converter. JSON feature commit

---
+++ src/main/java/com/qa/util/JSONUtil.java
@@ -0,0 +1,24 @@
+package com.qa.util;
+
+import com.google.gson.Gson;
+
+public class JSONUtil {
+	
+	private Gson gson;
+	
+	public JSONUtil() {
+		
+		this.gson = new Gson();
+	}
+	
+	public String getJSONForObject(Object obj) {
+		
+		return gson.toJson(obj);
+	}
+	
+	public <T> T getObjectforJSON(String jsonString, Class<T> clazz) {
+		
+		return gson.fromJson(jsonString, clazz);
+	}
+
+}
